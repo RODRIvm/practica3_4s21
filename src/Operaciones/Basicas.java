61bf468acee44fac5c606e05b98d981a5b89cb5c
CASA <CASA@192.168.1.77>
2022-04-06 13:07:16 -0500

    P3

---
+++ src/Operaciones/Basicas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Operaciones;
+
+/**
+ *
+ * @author rodri
+ */
+public class Basicas {
+    
+    public int Sumar(int num1, int num2){
+        return (num1+num2);
+    }
+    
+    public int Resta(int a, int b){
+        return (a-b);
+    }
+    
+}
